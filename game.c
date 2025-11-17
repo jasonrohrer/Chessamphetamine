358b8fa3ec93fc0569601a05e7ec5a9fe37c61ae
Jason Rohrer <jasonrohrer@fastmail.fm>
2025-11-17 13:52:39 -0500

    Got basics in place for keyboard input (with not all keys mapped yet), a dynamic game image that reacts to keyboard, and linux-specific logging to STDOUT.

--- game.c
+++ game.c
@@ -4,3 +4,113 @@
 #include "mingin.h"
 
 
+
+enum UserAction {
+    QUIT,
+    JUMP
+    };
+
+
+
+static int boxPosX = -1;
+static int boxPosY = -1;
+static int boxW = 10;
+static int boxH = 10;
+static int boxVPerSecond = 50;
+static int boxDir = 1;
+
+static char initDone = 0;
+
+
+void minginGame_getScreenPixels( int inWide, int inHigh,
+                                 unsigned char *inRGBBuffer ) {
+    int numPixels = inWide * inHigh;
+    int p;
+    int x, y;
+    
+    int boxStartX = boxPosX - boxW / 2;
+    int boxStartY = boxPosY - boxH / 2;
+    
+    /* white background */
+    for( p = 0; p<numPixels; p++ ) {
+        int pix = p * 3;
+
+
+        inRGBBuffer[pix] = 255;
+        inRGBBuffer[pix + 1] = 255;
+        inRGBBuffer[pix + 2] = 255;
+        }
+
+    /* now draw black box */
+    for( y = boxStartY; y < boxStartY + boxH; y++ ) {
+        int rowStart = y * inWide * 3;
+        
+        for( x = boxStartX; x < boxStartX + boxW; x ++ ) {
+            int pix = rowStart + x * 3;
+            inRGBBuffer[pix] = 255;
+            inRGBBuffer[pix + 1] = 0;
+            inRGBBuffer[pix + 2] = 0;
+            }
+        }
+    
+    }
+
+
+static void gameInit( void );
+
+
+void minginGame_step( void ) {
+    int w, h;
+    int r;
+
+    if( ! initDone ) {
+        gameInit();
+        initDone = 1;
+        }
+
+    if( mingin_isButtonDown( QUIT ) ) {
+        mingin_log( "Got quit key\n" );
+        
+        mingin_quit();
+        return;
+        }
+
+    if( mingin_isButtonDown( JUMP ) ) {
+        boxPosY -= 1;
+        }
+    
+    
+    
+    mingin_getScreenSize( &w, &h );
+    r = mingin_getStepsPerSecond();
+    
+    if( boxPosX == -1 ) {
+        /* init position in screen center */
+        boxPosX = w / 2;
+        boxPosY = h / 2;
+        }
+    else {
+        /* move box */
+        int boxVPerStep = boxVPerSecond / r;
+        if( boxVPerStep < 1 ) {
+            boxVPerStep = 1;
+            }
+        boxPosX += boxDir * boxVPerStep;
+
+        if( boxDir > 0 && boxPosX > w ) {
+            boxDir = -1;
+            }
+        else if( boxDir < 0 && boxPosX < 0 ) {
+            boxDir = 1;
+            }
+        }
+    }
+
+
+static MinginButton quitMapping[] = { MGN_KEY_Q, MGN_KEY_ESCAPE, MGN_MAP_END };
+static MinginButton jumpMapping[] = { MGN_KEY_SPACE, MGN_MAP_END };
+
+static void gameInit( void ) {
+    mingin_registerButtonMapping( QUIT, quitMapping );
+    mingin_registerButtonMapping( JUMP, jumpMapping );
+    }
--- mingin.h
+++ mingin.h
@@ -70,7 +70,7 @@
 
 
 /*
-  First, the game itself must implement these functions.
+  First, the game itself must implement these THREE functions.
   These are called by the plaform, and the platform is in charge
   of doing any necessary color or sample format conversions with the
   data provided by the game.
@@ -86,8 +86,9 @@
   RGBRGBRGB... in row-major order, starting from the top left corner
   of the screen and going left-to-right and top-to-bottom.
 */
-void mingin_gameGetScreenPixels( int inWide, int inHigh,
-                                  unsigned char *inRGBBuffer );
+void minginGame_getScreenPixels( int inWide, int inHigh,
+                                 unsigned char *inRGBBuffer );
+
 
 /*
   Get the next buffer full of audio samples.
@@ -95,10 +96,11 @@ void mingin_gameGetScreenPixels( int inWide, int inHigh,
 
   inSampleBuffer will have inNumSamples * inNumChannels * 2 bytes
 */
-void mingin_gameGetAudioSamples( int inNumSamples,
-                                  int inNumChannels,
-                                  int inSamplesPerSecond,
-                                  unsigned char *inSampleBuffer );
+void minginGame_getAudioSamples( int inNumSamples,
+                                 int inNumChannels,
+                                 int inSamplesPerSecond,
+                                 unsigned char *inSampleBuffer );
+
 
 /*
   Steps to the next frame of the game.
@@ -110,9 +112,12 @@ void mingin_gameGetAudioSamples( int inNumSamples,
   or buffers of audio to advance things.
 
   The rate at which mingin_gameStep is called can be found by calling
-  mingin_stepsPerSecond()
+  mingin_getStepsPerSecond()
 */
-void mingin_gameStep( void );
+void minginGame_step( void );
+
+
+
 
 
 
@@ -126,8 +131,25 @@ void mingin_gameStep( void );
   What's the step rate, in steps per second, that the platform
   is running the game at?
 */
-int mingin_stepsPerSecond( void );
+int mingin_getStepsPerSecond( void );
 
+
+void mingin_getScreenSize( int *outW, int *outH );
+
+
+#define MGN_MAP_END 0
+
+typedef enum MinginButton {
+    MGN_DUMMY_FIRST_BUTTON = MGN_MAP_END,
+    MGN_KEY_Q,
+    MGN_KEY_SPACE,
+    MGN_KEY_ESCAPE,
+    MGN_DUMMY_LAST_BUTTON
+    } MinginButton;
+
+#define MGN_NUM_BUTTONS  MGN_DUMMY_LAST_BUTTON
+
+    
 
 /*
   Registers a set of button types that should get mapped to one button
@@ -143,10 +165,27 @@ int mingin_stepsPerSecond( void );
 
   The game can also use this to map mutiple controls on the same platform
   to the same action constant.  For example, if FIRE can be triggered
-  by both { MGN_BUTTON_MOUSE_LEFT, MGN_KEY_X, MGN_MAP_END }
+  by both left mouse button and the X keyboard key with
+  { MGN_BUTTON_MOUSE_LEFT, MGN_KEY_X, MGN_MAP_END }
+
+  inButtonHandle can be in the range 0..255, which means 256 distinct
+  user actions can be mapped to buttons.
+
+  However, these can be remapped as many times as needed, allowing for
+  modal changes to controls, giving way more than 256 user actions if needed.
+
+  inMapping can contain at most 16 elements, including the final MGN_MAP_END
+
+  If there are more than 15 non-END elements in a mapping, the extra ones
+  will be ignored.
+  
+  Returns 1 on success, or 0 on failure (if inButtonHandle is out of
+  the supported range)
 */
-void mingin_registerButtonMapping( int inButtonHandle,
-                                    const int inMapping[] );
+char mingin_registerButtonMapping( int inButtonHandle,
+                                   const MinginButton inMapping[] );
+
+
 
 /*
   Check whether a previously-mapped button handle is currently held down.
@@ -156,7 +195,7 @@ char mingin_isButtonDown( int inButtonHandle );
 
 
 /*
-  Gets the current pointer location, if any.
+  Gets the current on-screen pointer location, if any.
   Point location is in screen space returned by mingin_getScreenSize(),
   with 0,0 at the top left corner of the screen.
 
@@ -172,19 +211,140 @@ char mingin_getPointerLocation( int *inX, int *inY );
   { MGN_LEFT_STICK_Y, MGN_MIDDLE_STICK_Y, MGN_MAP_END }
 */
 void mingin_registerStickAxis( int inStickAxisHandle,
-                                const int inMapping[] );
-
+                               const int inMapping[] );
 
 
 /*
-  Platforms must implement these functions, which the game can call
+  Writes a string to the log.
 */
+void mingin_log( const char *inString );
+
+
+/*
+  Exits the game program
+*/
+void mingin_quit( void );
+
+
+
 
 
 
 
 
 #ifdef MINGIN_IMPLEMENTATION
+
+
+/*
+  Platform must call this function once at startup.
+*/
+void minginInternal_init( void );
+
+
+/*
+  Platforms must implement these functions, which mingin will call internally.
+*/
+
+int minginPlatform_getStepsPerSecond( void );
+
+
+void minginPlatform_getScreenSize( int *outW, int *outH );
+
+void minginPlatform_quit( void );
+
+char minginPlatform_isButtonDown( MinginButton inButton );
+
+void minginPlatform_log( const char *inString );
+
+
+
+
+void mingin_getScreenSize( int *outW, int *outH ) {
+    minginPlatform_getScreenSize( outW, outH );
+    }
+
+
+int mingin_getStepsPerSecond( void ) {
+    return minginPlatform_getStepsPerSecond();
+    }
+
+
+void mingin_log( const char *inString ) {
+    minginPlatform_log( inString );
+    }
+
+
+void mingin_quit( void ) {
+    minginPlatform_quit();
+    }
+
+
+#define MINGIN_NUM_BUTTON_MAPPINGS 256
+#define MINGIN_MAX_BUTTON_MAPPING_ELEMENTS 16
+
+static MinginButton minginButtonMappings[ MINGIN_NUM_BUTTON_MAPPINGS ]
+                                        [ MINGIN_MAX_BUTTON_MAPPING_ELEMENTS ];
+
+
+void minginInternal_init( void ) {
+    int i;
+    for( i=0; i< MINGIN_NUM_BUTTON_MAPPINGS; i++ ) {
+        minginButtonMappings[ i ][ 0 ] = MGN_MAP_END;
+        }
+    }
+
+
+char mingin_registerButtonMapping( int inButtonHandle,
+                                   const MinginButton inMapping[] ) {
+    int i = 0;
+    
+    if( inButtonHandle < 0 || inButtonHandle >= MINGIN_NUM_BUTTON_MAPPINGS ) {
+        return 0;
+        }
+
+    /* leave room at end of our internal mapping for MGN_MAP_END */
+    while( i < MINGIN_MAX_BUTTON_MAPPING_ELEMENTS - 1
+           &&
+           inMapping[ i ] != MGN_MAP_END ) {
+
+        if( inMapping[i] <= MGN_DUMMY_FIRST_BUTTON ||
+            inMapping[i] >= MGN_DUMMY_LAST_BUTTON ) {
+            /* out of enum range
+               end the mapping now */
+            break;
+            }
+        minginButtonMappings[ inButtonHandle ][i] = inMapping[i];
+        i++;
+        }
+    
+    /* terminate */
+    minginButtonMappings[ inButtonHandle ][i] = MGN_MAP_END;
+    
+    
+    
+    return 1;
+    }
+
+
+char mingin_isButtonDown( int inButtonHandle ) {
+    int i = 0;
+    
+    if( inButtonHandle < 0 || inButtonHandle >= MINGIN_NUM_BUTTON_MAPPINGS ) {
+        return 0;
+        }
+    
+    while( minginButtonMappings[ inButtonHandle ][i] != MGN_MAP_END ) {
+        if( minginPlatform_isButtonDown(
+                minginButtonMappings[ inButtonHandle ][i] ) ) {
+            return 1;
+            }
+        i++;
+        }
+    
+    /* got through all possible buttons in mapping, and none
+       are considered pressed by the platform */
+    return 0;
+    }
 
 
 
@@ -208,9 +368,88 @@ void mingin_registerStickAxis( int inStickAxisHandle,
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <time.h>
+#include <unistd.h>
 
+/* x11 buffer is RGBA */
 static unsigned char screenBuffer[ WIN_W * WIN_H * 4 ];
 
+/* game's expected buffer is RGB */
+static unsigned char gameScreenBuffer[ WIN_W * WIN_H * 3 ];
+
+static char shouldQuit = 0;
+
+
+
+void minginPlatform_getScreenSize( int *outW, int *outH ) {
+    *outW = WIN_W;
+    *outH = WIN_H;
+    }
+
+
+int minginPlatform_getStepsPerSecond( void ) {
+    return LINUX_TARGET_FPS;
+    }
+
+
+void minginPlatform_quit( void ) {
+    shouldQuit = 1;
+    }
+
+
+/* status tracking pressed/released state */
+static char buttonDown[ MGN_NUM_BUTTONS ];
+
+/* maps each Mingin key to an X11 XK_ keysym */
+static KeySym buttonToXKeyMap[ MGN_NUM_BUTTONS ];
+
+/* fixme:
+   still need to handle controller input on Linux */
+
+char minginPlatform_isButtonDown( MinginButton inButton ) {
+    if( inButton <= MGN_DUMMY_FIRST_BUTTON ||
+        inButton >= MGN_DUMMY_LAST_BUTTON ) {
+        return 0;
+        }
+    
+    if( buttonDown[ inButton ] ) {
+        return 1;
+        }
+    
+    return 0;
+    }
+
+
+static MinginButton mapXKeyToButton( KeySym inXKey ) {
+    int i;
+    for( i=0; i<MGN_NUM_BUTTONS; i++ ) {
+        if( buttonToXKeyMap[ i ] == inXKey ) {
+            return i;
+            }
+        }
+    return MGN_DUMMY_FIRST_BUTTON;
+    }
+
+    
+
+
+static unsigned int stringLength( const char *inString );
+
+    
+
+static unsigned int stringLength( const char *inString ) {
+    unsigned int i = 0;
+    while( inString[i] != '\0' ) {
+        i++;
+        }
+    return i;
+    }
+        
+
+void minginPlatform_log( const char *inString ) {
+    write( STDOUT_FILENO, inString, stringLength( inString ) );
+    }
+
+    
 
 static void frameSleep( void );
 
@@ -256,8 +495,22 @@ int main( void ) {
     long unsigned int xBlackColor;
     long unsigned int xWhiteColor;
     GC xGc;
-    unsigned char nextColorVal = 255;
-        
+    int b;
+    
+    minginInternal_init();
+
+    for( b=0; b<MGN_NUM_BUTTONS; b++ ) {
+        buttonDown[b] = 0;
+        buttonToXKeyMap[b] = 0;
+        }
+    /* fixme
+       map all keys */
+    buttonToXKeyMap[ MGN_KEY_Q ] = XK_q;
+    buttonToXKeyMap[ MGN_KEY_ESCAPE ] = XK_Escape;
+    buttonToXKeyMap[ MGN_KEY_SPACE ] = XK_space;
+
+    
+    
     xDisplay = XOpenDisplay( NULL );
 
     xScreen = DefaultScreen( xDisplay );
@@ -272,7 +525,7 @@ int main( void ) {
         0, 0, WIN_W, WIN_H, 0, xBlackColor, xBlackColor );
 
     XSelectInput( xDisplay, xWindow,
-                  StructureNotifyMask | KeyPressMask | KeyReleaseMask);
+                  StructureNotifyMask | KeyPressMask | KeyReleaseMask );
 
     XMapWindow( xDisplay, xWindow );
 
@@ -295,36 +548,66 @@ int main( void ) {
                            (char *)screenBuffer, WIN_W, WIN_H,
                            32, 0 );
 
-    while( 1 ) {
+    while( ! shouldQuit ) {
         int numPixels = WIN_W * WIN_H;
         int i;
         
         /* pump all events */
-        while( XPending( xDisplay) > 0 ) {
+        while( XPending( xDisplay ) > 0 ) {
             XEvent e;
             XNextEvent( xDisplay, &e );
+
+            if( e.type == KeyPress ) {
+                
+                KeySym ks = XLookupKeysym( &(e.xkey), 0 );
+                
+                MinginButton button = mapXKeyToButton( ks );
+                
+                if( b > MGN_DUMMY_FIRST_BUTTON ) {
+                    buttonDown[ button ] = 1;
+                    }
+                }
+            else if( e.type == KeyRelease ) {
+                KeySym ks = XLookupKeysym( &(e.xkey), 0 );
+                
+                MinginButton button = mapXKeyToButton( ks );
+                
+                if( b > MGN_DUMMY_FIRST_BUTTON ) {
+                    buttonDown[ button ] = 0;
+                    }
+                }
             }
 
+        minginGame_step();
+        
+        minginGame_getScreenPixels( WIN_W, WIN_H, gameScreenBuffer );
         
         for( i=0; i<numPixels; i++ ) {
             int p = i * 4;
-            screenBuffer[ p ] = nextColorVal;
-            screenBuffer[ p + 1 ] = nextColorVal;
-            screenBuffer[ p + 2 ] = nextColorVal;
+            int gp = i * 3;
+            
+            screenBuffer[ p ] = gameScreenBuffer[ gp ];
+            screenBuffer[ p + 1 ] = gameScreenBuffer[ gp + 1 ];
+            screenBuffer[ p + 2 ] = gameScreenBuffer[ gp + 2 ];
             screenBuffer[ p + 3 ] = 255;
-            }
-        if( nextColorVal == 255 ) {
-            nextColorVal = 0;
-            }
-        else {
-            nextColorVal = 255;
             }
         
         XPutImage( xDisplay, xWindow, xGc, xImage, 0, 0, 0, 0, WIN_W, WIN_H );
         frameSleep();
         }
 
+    /* data is static
+       don't let xlib try destroying it */
+    xImage->data = NULL;
+    
     XDestroyImage( xImage );
+
+    XFreeGC( xDisplay, xGc );
+
+    XDestroyWindow( xDisplay, xWindow );
+
+    XCloseDisplay( xDisplay );
+    
     
     return 1;
     }

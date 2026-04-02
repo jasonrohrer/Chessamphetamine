befc77a313c771d6a8b60160e79f384f2d937f32
Jason Rohrer <jasonrohrer@fastmail.fm>
2026-04-02 10:17:47 -0400

    Added black and white board squares.

--- board.h
+++ board.h
@@ -29,6 +29,10 @@ void boardDraw( int  inCenterX,
 static  int  borderSpriteH;
 static  int  borderSpriteV;
 
+static  int  squareSpriteWhite;
+static  int  squareSpriteBlack;
+
+static  int  squareSize          =  25;
 
 
 void boardInit( void ) {
@@ -45,11 +49,73 @@ void boardInit( void ) {
     maxigin_initMakeGlowSprite( borderSpriteV,
                                 4,
                                 2 );
+
+    squareSpriteWhite = maxigin_initSprite( "squareWhite.tga" );
+
+    maxigin_initMakeGlowSprite( squareSpriteWhite,
+                                4,
+                                2 );
+
+    squareSpriteBlack = maxigin_initSprite( "squareBlack.tga" );
+
+    maxigin_initMakeGlowSprite( squareSpriteBlack,
+                                4,
+                                2 );
     }
 
 
 void boardDraw( int  inCenterX,
                 int  inCenterY ) {
+
+    int  y;
+    int  x;
+    int  pass;
+    
+    int  w   =  1;
+    
+    maxigin_drawSetColor( 0,
+                          255,
+                          255,
+                          255 );
+
+    /* draw black squares in first pass, then white on top */
+    for( pass = 0;
+         pass < 2;
+         pass ++ ) {
+        
+        for( y = 0;
+             y < 8;
+             y ++ ) {
+
+            int  yPos  =  inCenterY - 100 + y * squareSize + squareSize / 2;
+
+            for( x = 0;
+                 x < 8;
+                 x ++  ) {
+
+                int  xPos  =  inCenterX - 100 + x * squareSize + squareSize / 2;
+
+                if( w ) {
+                    if( pass == 1 ) {
+                        maxigin_drawSprite( squareSpriteWhite,
+                                            xPos,
+                                            yPos );
+                        }
+                    }
+                else {
+                    if( pass == 0 ) {
+                        maxigin_drawSprite( squareSpriteBlack,
+                                            xPos,
+                                            yPos );
+                        }
+                    }
+                w = ! w;
+                }
+            /* after each row, re-toggle */
+            w = ! w;
+            }
+        }
+    
 
     maxigin_drawSprite( borderSpriteH,
                         inCenterX,
@@ -66,6 +132,8 @@ void boardDraw( int  inCenterX,
     maxigin_drawSprite( borderSpriteV,
                         inCenterX + 99,
                         inCenterY );
+
+    maxigin_drawResetColor();
     }
 
 

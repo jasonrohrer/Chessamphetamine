80098fc64afb30c8c08e7d338b1da2a6e8ec8e62
Jason Rohrer <jasonrohrer@fastmail.fm>
2026-04-23 13:40:22 -0400

    Got all timing and phases of laser rook stepping and drawing working.  Still missing drawing/animating laser beams.

--- moveAnim.h
+++ moveAnim.h
@@ -56,7 +56,7 @@ static  int  beepUp       =  -1;
 static  int  beepDown     =  -1;
 static  int  shooshGood   =  -1;
 static  int  splatterBad  =  -1;
-
+static  int  laserSound   =  -1;
 
 
 void moveAnimInit( void ) {
@@ -65,6 +65,7 @@ void moveAnimInit( void ) {
     beepDown = maxigin_initSoundEffect( "beepDown.wav" );
     shooshGood = maxigin_initSoundEffect( "shooshGood.wav" );
     splatterBad = maxigin_initSoundEffect( "splatterBad.wav" );
+    laserSound = maxigin_initSoundEffect( "laser_sd_16.wav" );
     }
 
 
@@ -211,7 +212,6 @@ static char defaultPieceStep( BoardState  *inState,
 
 
 
-/* draws entire board with move animation according to inMoveProgress */
 static void defaultPieceDraw( int          inBoardCenterX,
                               int          inBoardCenterY,
                               BoardState  *inState,
@@ -280,6 +280,274 @@ static void defaultPieceDraw( int          inBoardCenterX,
 
 
 
+/* push this way above progress range used by a default move,
+   which would be diagonal pixels across board plus 512 for explosion */
+static int  laserStart  =  10000;
+static int  laserMax    =  10000 + 512;
+
+
+
+/* gets state and captured list for laser rook after it moves but
+   before it fires */
+static void getLaserRookMidState( BoardState  *inState,
+                                  Move        *inMove,
+                                  Captured    *inCaptured,
+                                  BoardState  *outMidState,
+                                  Captured    *outMidCaptured ) {
+
+    *outMidState    = *inState;
+    *outMidCaptured = *inCaptured;
+
+    if( outMidCaptured->num > 0 ) {
+
+        /* cut off all but the first one,
+           extras are always laser hits */
+
+        outMidCaptured->num = 1;
+
+        if( outMidCaptured->pieces[ 0 ].row != inMove->endPos[ 0 ]
+            ||
+            outMidCaptured->pieces[ 0 ].col != inMove->endPos[ 1 ] ) {
+
+            /* even the single capture is hit at a distance */
+            outMidCaptured->num = 0;
+            }
+        }
+
+    /* put rook where it lands in outMidState */
+    outMidState->grid[ inMove->startPos[0] ][ inMove->startPos[1] ] = noPiece;
+    outMidState->grid[ inMove->endPos  [0] ][ inMove->endPos  [1] ] =
+        inState->grid[ inMove->startPos[0] ][ inMove->startPos[1] ];
+    }
+
+
+    
+
+static char laserRookStep( BoardState  *inState,
+                           Move        *inMove,
+                           Captured    *inCaptured,
+                           BoardState  *inNewState,
+                           int         *inMoveProgress ) {
+
+    static  BoardState  midState;
+    static  Captured    midCaptured;
+    
+    char  takeDefault       =  0;
+    int   r;
+    int   explodeProgress;
+
+    (void)inNewState;
+    
+    
+    if( *inMoveProgress < laserStart ) {
+        
+        char  baseMoveDone;
+
+        getLaserRookMidState( inState,
+                              inMove,
+                              inCaptured,
+                              &midState,
+                              &midCaptured );
+
+        /* use base move for rook move and for any direct-piece capture
+           explosion.
+           Fire lasers after that's all done */
+        
+        baseMoveDone = defaultPieceStep( inState,
+                                         inMove,
+                                         &midCaptured,
+                                         &midState,
+                                         inMoveProgress );
+        if( !baseMoveDone ) {
+            return 0;
+            }
+
+        
+        if( inCaptured->num == 0 ) {
+            takeDefault = 1;
+            }
+        else if( inCaptured->num == 1 ) {
+
+            BoardPiece  bp  =  inCaptured->pieces[0];
+
+            if( bp.row == inMove->endPos[0]
+                &&
+                bp.col == inMove->endPos[1] ) {
+
+                /* single piece captured directly by rook, not by laser */
+
+                takeDefault = 1;
+                }
+            }
+
+        if( takeDefault ) {
+            /* done after default is done, no lasers fired */
+            return 1;
+            }
+        
+        /* start lasers on next step */
+        *inMoveProgress = laserStart;
+
+        maxigin_playSoundEffect( laserSound,
+                                 512 );
+        
+        return 0;
+        }
+    
+
+
+    r = mingin_getStepsPerSecond();
+
+    if( *inMoveProgress < laserMax ) {
+        /* step laser animation itself */
+        
+        *inMoveProgress += ( 30 * 60 ) / r;
+
+        if( *inMoveProgress >= laserMax ) {
+            /* start post-laser explosions on next step */
+            *inMoveProgress = laserMax;
+
+            if( inState->nextToMove == CHESS_WHITE ) {
+                
+                maxigin_playSoundEffect( shooshGood,
+                                         512 );
+                }
+            else {
+                maxigin_playSoundEffect( splatterBad,
+                                         512 );
+                }
+            }
+
+        return 0;
+        }
+
+    /* now explode pieces hit by laser */
+
+    explodeProgress = *inMoveProgress - laserMax;
+
+    explodeProgress = stepExplodingPiece( explodeProgress );
+
+    if( explodeProgress == -1 ) {
+        /* done exploding */
+        return 1;
+        }
+
+    *inMoveProgress = laserMax + explodeProgress;
+
+    return 0;
+    }
+
+
+
+static void laserRookDraw( int          inBoardCenterX,
+                           int          inBoardCenterY,
+                           BoardState  *inState,
+                           Move        *inMove,
+                           Captured    *inCaptured,
+                           BoardState  *inNewState,
+                           int          inMoveProgress ) {
+
+    static  BoardState  midState;
+    static  Captured    midCaptured;
+
+    (void)inNewState;
+
+    if( inMoveProgress < laserMax ) {
+
+        getLaserRookMidState( inState,
+                              inMove,
+                              inCaptured,
+                              &midState,
+                              &midCaptured );
+        
+
+        if( inMoveProgress < laserStart ) {
+            
+            defaultPieceDraw( inBoardCenterX,
+                              inBoardCenterY,
+                              inState,
+                              inMove,
+                              &midCaptured,
+                              &midState,
+                              inMoveProgress );
+            }
+        else {
+
+            /* hold board still from now during laser fire */
+
+            boardDraw( inBoardCenterX,
+                       inBoardCenterY );
+            
+            drawBoardState( &midState,
+                            0,
+                            0,
+                            0,
+                            0,
+                            inMove,
+                            0,
+                            0,
+                            inBoardCenterX,
+                            inBoardCenterY );
+
+            /* fixme:  draw the laser beams here! */
+
+            }
+        
+
+        return;
+        }
+
+    /* got here, we're done with laser fire, explode those pieces */
+
+    boardDraw( inBoardCenterX,
+               inBoardCenterY );
+    
+    /* draw end state */
+    drawBoardState( inNewState,
+                    0,
+                    0,
+                    0,
+                    0,
+                    inMove,
+                    0,
+                    0,
+                    inBoardCenterX,
+                    inBoardCenterY );
+
+    if( inCaptured->num > 0 ) {
+
+        /* there are captured pieces, draw them */
+
+        int  pn;
+        int  explodingProgress  =  inMoveProgress - laserMax;
+            
+        for( pn = 0;
+             pn < inCaptured->num;
+             pn ++ ) {
+
+            BoardPiece  *bp  =  &( inCaptured->pieces[ pn ] );
+
+            /* don't explot any captured pieces that our rook landed
+               on directly, since those were already exploded by default move */
+            if( bp->row != inMove->endPos[0]
+                ||
+                bp->col != inMove->endPos[1] ) {
+             
+                drawExplodingPiece( bp->p,
+                                    inBoardCenterX,
+                                    inBoardCenterY,
+                                    bp->row,
+                                    bp->col,
+                                    explodingProgress );
+                }
+            }
+        }
+    
+    }
+
+
+
+
 
 static MoveAnimStepFunction stepFunctions[ NUM_CHESS_PIECES ] =
                                               { noPieceStep,
@@ -289,7 +557,7 @@ static MoveAnimStepFunction stepFunctions[ NUM_CHESS_PIECES ] =
                                                 defaultPieceStep,
                                                 defaultPieceStep,
                                                 defaultPieceStep,
-                                                defaultPieceStep,
+                                                laserRookStep,
                                                 defaultPieceStep };
 
 
@@ -301,7 +569,7 @@ static MoveAnimDrawFunction drawFunctions[ NUM_CHESS_PIECES ] =
                                                 defaultPieceDraw,
                                                 defaultPieceDraw,
                                                 defaultPieceDraw,
-                                                defaultPieceDraw,
+                                                laserRookDraw,
                                                 defaultPieceDraw };
 
 

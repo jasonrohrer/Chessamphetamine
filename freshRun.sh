2cce06c570f791d8997ca7f38fae7be612d0564e
Jason Rohrer <jasonrohrer@fastmail.fm>
2026-04-27 13:12:51 -0400

    Fresh run script clears playback resume settings files.

--- freshRun.sh
+++ freshRun.sh
@@ -1,4 +1,4 @@
-rm -f settings/maxigin_save.bin
+rm -f settings/maxigin_save.bin settings/maxigin_resumePlayback.ini settings/maxigin_resumePlaybackPos.ini
 
 cp settings/maxigin_recording.bin settings/maxigin_playback.bin
 

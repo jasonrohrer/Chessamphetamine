e6d98e1318b1a45c33757416ffb3861d9fb31452
Jason Rohrer <jasonrohrer@fastmail.fm>
2026-09-07 22:57:20 -0400

    Removed vestigial line of code from windows long conversion script.

--- matchWindowsLongs.sh
+++ matchWindowsLongs.sh
@@ -1,7 +1,6 @@
 sed -i "s/unsigned long/unsigned int/g" *.h *.c
 sed -i "s/ long / int /g" *.h *.c
 sed -i "s/(long)/(int)/g" *.h *.c
-#sed -i "s/UL/U/g" *.h *.c
 sed -i "s/long mingin_/int mingin_/g" mingin.h
 sed -i "s/int            result  = syscall/long           result  = syscall/" mingin.h
 sed -i  "s/unsigned int            xBlackColor/unsigned long           xBlackColor/" mingin.h
